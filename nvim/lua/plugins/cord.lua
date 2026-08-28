595a3234a209392262ce50a9e573e7fcd3f39869
corvainx <ishmeetsingharora07@gmail.com>
2026-08-28 15:31:37 +0530

    files

---
+++ nvim/lua/plugins/cord.lua
@@ -0,0 +1,13 @@
+return {
+	"vyfor/cord.nvim",
+	build = "./build",
+	event = "VeryLazy",
+	opts = {
+		editor = {
+			client = "neovim",
+		},
+		display = {
+			theme = "catppuccin",
+		},
+	},
+}
